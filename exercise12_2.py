2725801fde59a94a3233de1cbad5701dbc72f950
Ivon Martinez <ivon.mrtnz@gmail.com>
2025-02-09 12:15:39 +0000

    Edited exercise12_2 and 12_3

---
+++ exercise12_2.py
@@ -0,0 +1,13 @@
+# Exercise 12_2
+#Whats is going on here? Explain the output
+
+tup = 'Hello'
+print(len(tup))
+
+tup = 'Hello',
+print(len(tup))
+
+#The first one print 5 because it is a string, so it prints the numbers of characters of the string
+#The second one print 1 because it is a tuple and it only has one element
+#The difference is in the coma, which makes it a tuple
+
---
+++ exercise12_3.py
@@ -0,0 +1,11 @@
+#we have to import the random library
+import random
+#we need to store 6 uniques numbers, and for that reason we use set as it will not have repeated numbers
+lotto_numbers = set()
+# we have to add 6 numbers to our lotto variable, so we use the while loop to make sure that
+# once lotto_numbers has 6 elements then it will get out of the loop
+while len(lotto_numbers) < 6:
+#we want them to be random numbers, so that's why we use random.randint,
+# which takes two arguments to define the range as we want it to go from 1 to 50, both numbers are inclusive
+    lotto_numbers.add(random.randint(1, 50))
+print(f"Your lotto ticket is: {lotto_numbers}")
